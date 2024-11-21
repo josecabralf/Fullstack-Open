c56138767ef63f561f3dc502b4f02aa2dd3dfe9b
José Cabral <cabraljose1210@gmail.com>
2024-11-20 22:30:57 -0300

    1.7: Unicafe step 2

--- part1/cafe-feedback/src/App.jsx
+++ part1/cafe-feedback/src/App.jsx
@@ -4,8 +4,9 @@ const Header = ({ text }) => <h2>{text}</h2>
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
+const Statistic = ({ text, value }) => <div>{text} {value}</div>
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -32,9 +33,16 @@ const App = () => {
 
       <div id="statistics">
         <Header text="statistics" />
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
+        <div id='basic-statistics'>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+        </div>
+        <div id='complex-statistics'>
+          <Statistic text="all" value={good + neutral + bad} />
+          <Statistic text="average" value={(good - bad)/(good + neutral + bad) || '0'} />
+          <Statistic text="positive" value={`${good/(good + neutral + bad) * 100 || '0'}%`} />
+        </div>
       </div>
     </div>
   )
