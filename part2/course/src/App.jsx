aaf89c0b0cccade081ba787b66a07f85c888cd86
José Cabral <cabraljose1210@gmail.com>
2024-11-22 18:03:00 -0300

    Started part 2

---
+++ part2/course/src/App.jsx
@@ -0,0 +1,66 @@
+class CourseModel {
+  constructor(name, parts) {
+    this.name = name;
+    this.parts = parts;
+  }
+
+  total() {
+    return this.parts.reduce((acc, part) => acc + part.exercises, 0);
+  }
+}
+
+class CoursePartModel {
+  constructor(name, exercises) {
+    this.name = name;
+    this.exercises = exercises;
+  }
+}
+
+const Header = ({name}) => <h1>{name}</h1>;
+
+const Part = ({part}) => {
+  const { name, exercises } = part;
+
+  return (
+    <p>{name} {exercises}</p>
+  );
+}
+
+const Content = ({ parts }) => {
+  const renderParts = () => parts.map(part => <Part key={part.name} part={part} />);
+
+  return (
+    <div>
+      {renderParts()}
+    </div>
+  )
+}
+
+const Total = ({total}) => <p><b>Number of exercises: {total}</b></p>;
+
+const CoursePage = ({course}) => 
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total total={course.total()} />
+    </div>
+
+
+const App = () => {
+  const course = new CourseModel(
+    'Half Stack application development', 
+    [
+      new CoursePartModel('Fundamentals of React', 10),
+      new CoursePartModel('Using props to pass data', 7),
+      new CoursePartModel('State of a component', 14)
+    ]
+  );
+
+  return (
+    <div>
+      <CoursePage course={course} />
+    </div>
+  );
+}
+
+export default App;
