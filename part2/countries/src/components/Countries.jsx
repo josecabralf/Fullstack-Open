69363495856866fb51a6adaaf30afd76403da686
José Cabral <cabraljose1210@gmail.com>
2024-11-24 01:03:55 -0300

    2.19*: Data for countries, step 2

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,3 +1,7 @@
+import { useState } from 'react';
+
+export const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>
+
 export const TooMany = () => {
   return (
     <div>Too many matches, specify another filter</div>
@@ -7,14 +11,6 @@ export const TooMany = () => {
 export const None = () => {
   return (
     <div>No matches</div>
-  );
-}
-
-export const CountriesList = ({ countries }) => {
-  return (
-    <div>
-      {countries.map(country => <div key={country.name.common}>{country.name.common}</div>)}
-    </div>
   );
 }
 
@@ -32,5 +28,23 @@ export const Country = ({ country }) => {
       </ul>
       <img src={country.flags.png} alt={country.name.common} style={{ width: '200px' }} />
       </div>
+  );
+}
+
+export const CountriesList = ({ countries }) => {
+  const [selected, setSelected] = useState();
+
+  return (
+    <div>
+      {countries.map(country => 
+        <div key={country.name.common}>
+          {country.name.common}
+          {selected !== country 
+            ? <Button text={'show'} onClick={() => setSelected(country)} />
+            : <Button text={'hide'} onClick={() => setSelected()} />
+          }
+          {selected === country && <Country country={selected} />}
+        </div>)}
+    </div>
   );
 }
