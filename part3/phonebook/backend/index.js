432c8d5f255de11ec822945dfe6b3ff5bc93ac42
José Cabral <cabraljose1210@gmail.com>
2024-11-27 19:42:01 -0300

    setting up part 3.11

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const cors = require('cors');
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'))
 // app.use(morgan('tiny'));
 // morgan.token('data', (req, res) =>  req.method === 'POST' ? JSON.stringify(req.body) : '');
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
