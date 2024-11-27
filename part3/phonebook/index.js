06b33dfc6393f9a887246721c9b1ca362b798062
José Cabral <cabraljose1210@gmail.com>
2024-11-27 18:39:26 -0300

    3.8: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
-
 const morgan = require('morgan');
 
+const app = express();
+
 app.use(express.json());
-app.use(morgan('tiny'));
+
+// app.use(morgan('tiny'));
+morgan.token('data', (req, res) =>  req.method === 'POST' ? JSON.stringify(req.body) : '');
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
 let persons = require('./data/persons').persons;
 
