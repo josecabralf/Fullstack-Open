019f89414c10c1085ef7ff3b79092a6a0bd27b02
José Cabral <cabraljose1210@gmail.com>
2024-11-27 18:09:39 -0300

    3.3: Phonebook backend step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,16 +5,26 @@ app.use(express.json());
 
 const persons = require('./data/persons').persons;
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons);
-});
-
 app.get('/info', (req, res) => {
     const date = new Date();
     res.send(
         `<p>Phonebook has info for ${persons.length} people</p>
         <p>${date}</p>`
     );
+});
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(person => person.id === id);
+
+    if (!person)
+        return res.status(404).end();
+
+    res.json(person);
 });
 
 const PORT = 3001;
