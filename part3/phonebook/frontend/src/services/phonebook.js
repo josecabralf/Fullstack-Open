754884616ddb899657ade1893331019d8922648a
José Cabral <cabraljose1210@gmail.com>
2024-11-27 19:19:43 -0300

    3.10 Phonebook backend step 10 - changed baseUrl

--- part3/phonebook/frontend/src/services/phonebook.js
+++ part3/phonebook/frontend/src/services/phonebook.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "https://fullstack-open-cqwj.onrender.com/api/persons";
 
 export const getAll = () => axios.get(baseUrl).then(resp => resp.data);
 export const create = (newPerson) => axios.post(baseUrl, newPerson).then(resp => resp.data);
