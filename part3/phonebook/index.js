1a42d90d1a9741864667dd7abe1bf40d5857feab
José Cabral <cabraljose1210@gmail.com>
2024-11-27 18:06:42 -0300

    3.2: Phonebook backend step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -9,6 +9,14 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port http://localhost:${PORT}/`)
+app.get('/info', (req, res) => {
+    const date = new Date();
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>`
+    );
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port http://localhost:${PORT}/`);
