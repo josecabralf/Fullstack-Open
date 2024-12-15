67d3c4d7ba877a46e8031a9c79544ab74e821502
José Cabral <cabraljose1210@gmail.com>
2024-12-15 16:28:41 -0300

    4.12: Blog List tests, step 5 - adding test levels

--- part4/blog/tests/blogs_api.test.js
+++ part4/blog/tests/blogs_api.test.js
@@ -7,65 +7,69 @@ const app = require('../app');
 
 const api = supertest(app);
 
-beforeEach(async () => await helper.resetDb());
+describe('when there are some blogs saved initially', () => {
+  beforeEach(async () => await helper.resetDb());
 
-describe('blogs', () => {
-  test('returns correct amount of posts', async () => {
-    const response = await api.get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
-
-    assert.strictEqual(response.body.length, helper.initialBlogs.length);
+  describe('viewing all blog posts', () => {
+    test('returns correct amount of posts', async () => {
+      const response = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+  
+      assert.strictEqual(response.body.length, helper.initialBlogs.length);
+    });
+  
+    test('has id property', async () => {
+      const response = await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+  
+      assert(response.body[0].id);
+    });
   });
 
-  test('has id property', async () => {
-    const response = await api.get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/);
+  describe('creating a new blog post', () => {
+    test('creates a new blog post', async () => {
+      const response = await api.post('/api/blogs')
+        .send(helper.newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-    assert(response.body[0].id);
-  });
+      const blogsInDb = await helper.blogsInDb();
+      assert.strictEqual(blogsInDb.length, helper.initialBlogs.length + 1);
 
-  test('creates a new blog post', async () => {
-    const response = await api.post('/api/blogs')
-      .send(helper.newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
+      const { id, ...addedBlog } = blogsInDb[2]; // Check if the new blog post has the correct values
+      assert.deepStrictEqual(addedBlog, helper.newBlog);
+    });
 
-    const blogsInDb = await helper.blogsInDb();
-    assert.strictEqual(blogsInDb.length, helper.initialBlogs.length + 1);
+    test('likes property defaults to 0', async () => {
+      const blogWithoutLikes = { ...helper.newBlog, likes: undefined };
+      const response = await api.post('/api/blogs')
+        .send(blogWithoutLikes)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-    const { id, ...addedBlog } = blogsInDb[2]; // Check if the new blog post has the correct values
-    assert.deepStrictEqual(addedBlog, helper.newBlog);
-  });
+      const { likes } = response.body;
+      assert.strictEqual(likes, 0);
+    });
 
-  test('likes property defaults to 0', async () => {
-    const blogWithoutLikes = { ...helper.newBlog, likes: undefined };
-    const response = await api.post('/api/blogs')
-      .send(blogWithoutLikes)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
+    test('returns 400 if title is missing', async () => {
+      const blogWithoutTitle = { ...helper.newBlog, title: undefined };
+      await api.post('/api/blogs')
+        .send(blogWithoutTitle)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+        .expect({ error: 'Title is required' });      
+    });
 
-    const { likes } = response.body;
-    assert.strictEqual(likes, 0);
-  });
-
-  test('returns 400 if title is missing', async () => {
-    const blogWithoutTitle = { ...helper.newBlog, title: undefined };
-    await api.post('/api/blogs')
-      .send(blogWithoutTitle)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-      .expect({ error: 'Title is required' });      
-  });
-
-  test('returns 400 if url is missing', async () => {
-    const blogWithoutUrl = { ...helper.newBlog, url: undefined };
-    await api.post('/api/blogs')
-      .send(blogWithoutUrl)
-      .expect(400)
-      .expect('Content-Type', /application\/json/)
-      .expect({ error: 'Url is required' });
+    test('returns 400 if url is missing', async () => {
+      const blogWithoutUrl = { ...helper.newBlog, url: undefined };
+      await api.post('/api/blogs')
+        .send(blogWithoutUrl)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+        .expect({ error: 'Url is required' });
+    });
   });
 });
 
