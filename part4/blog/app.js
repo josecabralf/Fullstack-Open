cf657e36ea0d0f5fa5eca49ca8d234cbd1076232
José Cabral <cabraljose1210@gmail.com>
2024-12-15 16:14:08 -0300

    4.10: Blog List Tests, step 3 - clean up code

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,12 +1,12 @@
 const config = require('./utils/config');
 const express = require('express');
+require('express-async-errors');
+const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
-
-const app = express();
 
 logger.info('connecting to mongodb');
 mongoose.set('strictQuery', false);
--- part4/blog/tests/blogs_api.test.js
+++ part4/blog/tests/blogs_api.test.js
@@ -2,39 +2,12 @@ const { test, after, beforeEach, describe } = require('node:test');
 const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const helper = require('./utils/blogs_test_helper');
 const app = require('../app');
-const Blog = require('../models/blog');
 
 const api = supertest(app);
 
-const initialBlogs = [
-  {
-    title: "Hello World",
-    author: "James A. Doe",
-    url: "http://www.jccodesblog.com",
-    likes: 3
-  },
-  {
-    title: "JS Basics",
-    author: "Fred K. Simmons",
-    url: "http://www.jccodesblog.com",
-    likes: 5
-  }
-];
-
-const newBlog = {
-  title: "React Hooks",
-  author: "Jane Popper",
-  url: "http://www.jccodesblog.com",
-  likes: 9
-};
-
-beforeEach(async () => {
-  await Blog.deleteMany({});
-
-  await new Blog(initialBlogs[0]).save();
-  await new Blog(initialBlogs[1]).save();
-});
+beforeEach(async () => await helper.resetDb());
 
 describe('blogs', () => {
   test('returns correct amount of posts', async () => {
@@ -42,28 +15,28 @@ describe('blogs', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/);
 
-    assert.strictEqual(response.body.length, initialBlogs.length);
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
   });
 
   test('has id property', async () => {
     const response = await api.get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/);
-      
+
     assert(response.body[0].id);
   });
 
   test('creates a new blog post', async () => {
     const response = await api.post('/api/blogs')
-      .send(newBlog)
+      .send(helper.newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/);
 
-    const blogsResponse = (await api.get('/api/blogs')).body; // Check if the new blog post was added
-    assert.strictEqual(blogsResponse.length, initialBlogs.length + 1);
+    const blogsInDb = await helper.blogsInDb();
+    assert.strictEqual(blogsInDb.length, helper.initialBlogs.length + 1);
 
-    const { id, ...addedBlog } = blogsResponse[2]; // Check if the new blog post has the correct values
-    assert.deepStrictEqual(addedBlog, newBlog);
+    const { id, ...addedBlog } = blogsInDb[2]; // Check if the new blog post has the correct values
+    assert.deepStrictEqual(addedBlog, helper.newBlog);
   });
 });
 
